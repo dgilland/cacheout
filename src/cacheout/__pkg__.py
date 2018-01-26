c618464a890cb3b3d1db862d8e9bd03fe61ab553
Derrick Gilland <dgilland@gmail.com>
2018-01-26 00:53:51 -0500

    Update pkg description.

--- src/cacheout/__pkg__.py
+++ src/cacheout/__pkg__.py
@@ -2,7 +2,7 @@
 """
 
 __package_name__ = 'cacheout'
-__description__ = 'Cache library'
+__description__ = 'A caching library for Python'
 __url__ = 'https://github.com/dgilland/cacheout'
 __version__ = '0.0.0'
 __author__ = 'Derrick Gilland'
