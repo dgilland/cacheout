966a1abba4b49771fb8b882b18f952e7a2165ead
Derrick Gilland <dgilland@gmail.com>
2018-01-28 23:59:14 -0500

    Release v0.1.0.

--- src/cacheout/__pkg__.py
+++ src/cacheout/__pkg__.py
@@ -1,10 +1,10 @@
-"""Define project metadata
+"""Project metadata
 """
 
 __package_name__ = 'cacheout'
 __description__ = 'A caching library for Python'
 __url__ = 'https://github.com/dgilland/cacheout'
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 __author__ = 'Derrick Gilland'
 __email__ = 'dgilland@gmail.com'
 __license__ = 'MIT License'
