b2212b38d9a4b17c7342f4c41c132217c42efe39
Derrick Gilland <dgilland@gmail.com>
2019-01-03 19:43:24 +0000

    Move setup.py options to setup.cfg. Move version from package into setup.cfg and dynamically fetch version inside package instead.

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
-package_dir = 'src'
-packages = find_packages(package_dir)
-
-version = {}
-with open('{}/{}/__version__.py'.format(package_dir, packages[0])) as f:
-    exec(f.read(), version)
-
-
-setup(
-    version=version['__version__'],
-    package_dir={'': package_dir},
-    packages=packages,
-    include_package_data=True
-)
+setup()
--- src/cacheout/__version__.py
+++ src/cacheout/__version__.py
@@ -1,4 +1,11 @@
 """Project version information.
 """
 
-__version__ = '0.11.0'
+from pkg_resources import get_distribution, DistributionNotFound
+
+
+try:
+    __version__ = get_distribution(__name__.split('.')[0]).version
+except DistributionNotFound:  # pragma: no cover
+    # Package is not installed.
+    __version__ = None
