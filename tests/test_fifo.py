53cb79236cf599e4d7a3607582270f47fa7e691d
Derrick Gilland <dgilland@gmail.com>
2022-08-16 09:25:21 -0400

    Support Python 3.10.

--- tests/test_fifo.py
+++ tests/test_fifo.py
@@ -6,7 +6,7 @@ from cacheout import Cache, FIFOCache
 def test_fifo_does_not_override_cache_class():
     """Test that FIFOCache doesn't override any methods in Cache."""
     for name, value in inspect.getmembers(FIFOCache):
-        if name in (
+        if not value or name in (
             "__doc__",
             "__module__",
             "__dict__",
